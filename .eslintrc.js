33c3d6483fe7b6d9e828491bb8bedd292bf98503
Dmitry Ivanov <di.ivanov2018@gmail.com>
2021-11-14 15:03:53 +0300

    Install prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,28 +2,25 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    'jest/globals': true,
+    "jest/globals": true,
   },
   extends: [
-    'airbnb-base',
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-
+    "airbnb-base",
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
-  plugins: [
-    'jest',
-    '@typescript-eslint',
-  ],
+  plugins: ["jest", "@typescript-eslint"],
   rules: {
-    'import/prefer-default-export': 'off',
-    'max-len': ['error', { ignoreComments: true }],
-    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
-    'import/no-unresolved': 'off',
-    'import/extensions': ['warn', 'never'],
+    "import/prefer-default-export": "off",
+    "max-len": ["error", { ignoreComments: true }],
+    "no-plusplus": ["error", { allowForLoopAfterthoughts: true }],
+    "import/no-unresolved": "off",
+    "import/extensions": ["warn", "never"],
   },
 };
